57cebec412bc43ef72411033b7a94b4a7c0af012
liuqingshang <liu-qingshang@caica.jp>
2019-11-19 10:32:41 +0900

    创建版本 1.0.0.

---
+++ TestLib001/TestLib001/Classes/TestLib001DataUtils.swift
@@ -0,0 +1,17 @@
+//
+//  TestLib001DataUtils.swift
+//  TestLib001
+//
+//  Created by CAICA on 2019/11/19.
+//  Copyright © 2019 nn. All rights reserved.
+//
+
+import Foundation
+
+
+class TestLib001DataUtils {
+    
+    class func testMethod() {
+        print("xxxx")
+    }
+}
---
+++ TestLib001/TestLib001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestLib001
+//
+//  Created by CAICA on 2019/11/19.
+//  Copyright © 2019 nn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
